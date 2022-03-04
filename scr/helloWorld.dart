f077a80787bb6fca753d75385a77b2efd6b1ca32
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-03-04 22:06:03 +0100

    Initial commit

---
+++ scr/helloWorld.dart
@@ -0,0 +1,15 @@
+
+
+void main() {
+  print('Hello, World!');
+  var name = 'Kabraxis';
+  String lastName = 'Luvos';
+  var eclipseDays = 365;
+  var pi = 3.14159;
+
+  print('Hi, my name is: $name and my last name is $lastName.');
+  print('The earth takes $eclipseDays days to obit the sun.');
+  print('Fun-fact: An opinion without "Pi" - which is $pi - is just an onion.');
+
+
+}
