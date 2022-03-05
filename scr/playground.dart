036ade05c418610f73fb62392f7f6a47d15a9b9b
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-03-06 00:18:55 +0100

    stdout.writeln, stdin.readLineSync();, var & dynamic examples

--- scr/playground.dart
+++ scr/playground.dart
@@ -3,14 +3,18 @@ import 'dart:io';
 import 'dart:typed_data';
 
 main() {
+
+  // stdout & stdin example
   stdout.writeln('What is your name?');
   String? name = stdin.readLineSync();
   print('My name is $name');
 
+  // var example
   double number1 = 20.94;
   var number2 = 309.27;
   print('number1: $number1 | number2: $number2');
 
+  // dynamic example
   dynamic amount = 100.57;
   print('Amount: $amount,');
 
