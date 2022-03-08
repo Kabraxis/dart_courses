f65fb5fa18a61926e17c35175236a8a0adf8a34c
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-03-08 21:41:49 +0100

    variables

--- scr/dartInFourHours.dart
+++ scr/dartInFourHours.dart
@@ -1,6 +1,13 @@
-void main(){
-  print("   /|");
-  print("  / |");
-  print(" /  |");
-  print("/___|");
+void main() {
+
+  String characterName = "Jim";
+  String characterAge = "35";
+
+  print("There one was a man named ${characterName}");
+  print("He was ${characterAge} years old");
+
+  characterName = "Dwight";
+
+  print("He really liked the name ${characterName}");
+  print("But he didn't like being $characterAge");
 }
