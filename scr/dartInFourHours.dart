a5881f0ff86d3c6a438f18fbf48ebcd1001b164e
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-03-09 20:25:49 +0100

    basic data types, indexes, string methods

--- scr/dartInFourHours.dart
+++ scr/dartInFourHours.dart
@@ -1,5 +1,25 @@
 void main() {
 
+  // Indexes
+  String greeting = "Hello";
+  //                 01234
+  String greeting2 = " World";
+  print(greeting.length); // Length is 5
+  print(greeting[0]); // Index 0
+  print(greeting.toUpperCase()); // HELLO
+  print(greeting.indexOf("e")); // 1
+  print(greeting.contains("e")); // true
+  print(greeting + greeting2); // Hello World
+  print("The greeting is: ${greeting + greeting2}"); // Hello World
+
+
+  // Data types basic
+  String firstName = "Kabraxis";
+  int age = 26;
+  double gpa = 3.5;
+  bool isRegisteredVoter = false;
+
+  // Strings and interpolation
   String characterName = "Jim";
   String characterAge = "35";
 
