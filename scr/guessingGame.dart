670524c55915f2fdb85ed28d3c56ac0be31e1044
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-03-11 16:22:56 +0100

    Name guessing game

---
+++ scr/guessingGame.dart
@@ -0,0 +1,26 @@
+import "dart:io";
+
+String prompt(String promptText) {
+  print(promptText);
+  String? answer = stdin.readLineSync();
+  return answer!;
+}
+
+double promptDouble() {
+  print("Enter a number: ");
+  double myNum = double.parse(stdin.readLineSync()!);
+  return myNum;
+}
+
+void main() {
+  String answer = "Kabraxis Luvos";
+  String guess = "";
+  int guessCount = 0;
+
+  while (guess != answer) {
+    guess = prompt("Enter a guess: ");
+    guessCount++;
+  }
+
+  print("You Won in ${guessCount} guesses!");
+}
