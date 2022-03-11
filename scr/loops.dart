3bdb22ab98b3d64f1aa5d62afc92ae8fd55437b3
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-03-11 19:16:52 +0100

    While, For, For in loops

--- scr/loops.dart
+++ scr/loops.dart
@@ -1,8 +1,34 @@
-void main() {
-  int i = 0;
+void drawTriangles(int numTriangles) {
+  for (int i = 0; i < numTriangles; i++) {
+    print(" /|");
+    print("/_|");
+  }
+}
 
+void main() {
+  List<String> friends = ["Stanley", "Pam", "Karen"];
+
+  for (String friend in friends) {
+    print(friend);
+  }
+
+  for (int i = 0; i < friends.length; i++) {
+    print(friends[i]);
+  }
+
+  int i = 0;
+  while (i < 3) {
+    print(friends[i]);
+    i++;
+  }
+
+  i = 0;
   while (i < 5) {
     print(i);
     i++;
   }
+
+
+    drawTriangles(2);
+
 }
