0ca7e449f1878d02ade96a00c47c3cecbff4b11a
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-03-12 21:01:29 +0100

    prompting user input, consider right answers, show % right answers

--- scr/mathQuiz.dart
+++ scr/mathQuiz.dart
@@ -1,3 +1,5 @@
+import "dart:io";
+
 class MathQuestion {
   String? question;
   double? answer;
@@ -8,6 +10,18 @@ class MathQuestion {
   }
 }
 
+String prompt(String promptText) {
+  print(promptText);
+  String? answer = stdin.readLineSync();
+  return answer!;
+}
+
+double promptDouble(String promptText) {
+  print(promptText);
+  double myNum = double.parse(stdin.readLineSync()!);
+  return myNum;
+}
+
 void main() {
   List<MathQuestion> questions = [
     MathQuestion("3 + 5", 8.0),
@@ -15,9 +29,19 @@ void main() {
     MathQuestion("100 * 9", 900.0)
   ];
 
-  for (MathQuestion mathQuestion in questions)  {
-    print(mathQuestion.question);
+  int score = 0;
+
+  for (MathQuestion mathQuestion in questions) {
+    double userAnswer = promptDouble(mathQuestion.question!);
+    if (userAnswer == mathQuestion.answer) {
+      print("Correct!");
+      score++;
+    } else {
+      print("Incorrect, the answer was ${mathQuestion.answer}");
+    }
   }
+
+  print("You got ${score / questions.length * 100}%");
 }
 /*
 Eine Classe ist nichts anderes als eine selbst definierte komplexe Variable.
